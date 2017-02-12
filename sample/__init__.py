529a7a0b0daf0f3eee8aa445cedf2eb561e089fe
Marouane BENALLA <marouan.benalla@gmail.com>
2017-02-12 12:26:49 +0100

    create the setup package

---
+++ sample/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+"""
+     @project : elastic-handler
+     @file    : __init__.py
+     @author  : Marouane BENALLA    
+     @email   : marouan.benalla@gmail.com
+     @company : bubble tech
+"""
